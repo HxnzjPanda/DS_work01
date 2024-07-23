14758225e856638a4b75e0bb3e80b8aea04044b0
HxnzjPanda <as2201021@gmail.com>
2024-07-23 14:48:57 +0800

    Create Ackermann_nonrecursive.cpp

---
+++ Ackermann_nonrecursive.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+    int m, n;
+    while (cin >> m >> n) {
+        int num = 0;
+        switch(m) {
+            case 0:
+                num = n + 1;
+                cout << num << endl;
+                break;
+            case 1:
+                num = n + 2;
+                cout << num << endl;
+                break;
+            case 2:
+                num = 2*(n+3)-3;
+                cout << num << endl;
+                break;
+            case 3:
+                num = pow(2,(n+3))-3;
+                cout << num << endl;
+                break;
+            case 4:
+                if (n == 0) {
+                    num = 13;
+                    cout << num << endl;
+                }
+                else if (n == 1) {
+                    num = 65533;
+                    cout << num << endl;
+                }
+                else
+                    cout << "n太大" << endl;
+                break;
+            case 5:
+                if (n == 0) {
+                    num = 65533;
+                    cout << num << endl;
+                }
+                else
+                    cout << "n太大" << endl;
+
+                break;
+        }
+        
+    }
+}
