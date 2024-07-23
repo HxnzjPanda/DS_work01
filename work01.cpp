1ac6b6385b4cea1d6789d8af9e1b2b3907fbcf48
HxnzjPanda <as2201021@gmail.com>
2024-07-23 14:17:54 +0800

    Update work01.cpp

--- work01.cpp
+++ work01.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <cstdilb>
+using namespace std;
+int A(int m,int n){
+  if(m==0){
+    return n+1;
+  }
+  else if(n==0){
+    return A(m-1,a);
+  }
+  else
+    return A(m-1,A(m,n-1));  
+}
+int main(){
+  int m,n;
+  while(cin>>m>>n){
+    cout>>A(m,n)<<endl;
+  }
+}
